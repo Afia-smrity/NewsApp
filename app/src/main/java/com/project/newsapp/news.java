d7c919909e49acba6686b3a8348a6c253fe6cba2
unknown <smrityislam8765@gmail.com>
2021-11-18 00:07:59 +0600

    welcome

---
+++ app/src/main/java/com/project/newsapp/news.java
@@ -0,0 +1,17 @@
+package com.project.newsapp;
+
+public class news {
+    int n;
+
+    public news(int n) {
+        this.n = n;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+}
